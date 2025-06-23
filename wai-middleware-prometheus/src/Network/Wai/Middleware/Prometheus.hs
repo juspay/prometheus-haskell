a83326fbb16fb1b5eaf0f9c4edd0eba7687d8bbb
Aneel <aneel.kumar@juspay.in>
2025-06-23 20:19:09 +0530

    Add limit for adding metrics in metric map

--- wai-middleware-prometheus/src/Network/Wai/Middleware/Prometheus.hs
+++ wai-middleware-prometheus/src/Network/Wai/Middleware/Prometheus.hs
@@ -50,8 +50,7 @@ instance Default.Default PrometheusSettings where
 
 {-# NOINLINE requestLatency #-}
 requestLatency :: Prom.Vector Prom.Label3 Prom.Histogram
-requestLatency = Prom.unsafeRegister $ Prom.vector ("handler", "method", "status_code")
-                                     $ Prom.histogram info Prom.defaultBuckets
+requestLatency = Prom.unsafeRegister $ Prom.vector ("handler", "method", "status_code") (Prom.histogram info Prom.defaultBuckets) Nothing
     where info = Prom.Info "http_request_duration_seconds"
                            "The HTTP request latencies in seconds."
 
