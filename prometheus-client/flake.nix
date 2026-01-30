dc21e077782efe0b7c9a7656840f4d4372c309cc
Kiransai.abhishek <kiransai.abhishek@juspay.in>
2026-01-30 13:22:54 +0530

    ghc984

---
+++ prometheus-client/flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs.url = "github:nixos/nixpkgs/89c2b2330e733d6cdb5eae7b899326930c2c0648";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake { inputs = inputs // { inherit (inputs) nixpkgs nixpkgs-latest; }; } {
+      systems = import inputs.systems;
+      imports = [inputs.haskell-flake.flakeModule];
+
+      perSystem = {
+        self',
+        pkgs,
+        lib,
+        config,
+        ...
+      }: {
+        haskellProjects.default = {
+          basePackages = pkgs.haskell.packages.ghc98;
+          packages = {
+            record-dot-preprocessor.source="0.2.17";
+            servant.source="0.20.2";
+          };
+          devShell = {
+            tools = hp: {
+              haskell-language-server = null;
+            };
+          };
+        };
+        packages.default =  self'.packages.ghc-hasfield-plugin;
+      };
+    };
+}
